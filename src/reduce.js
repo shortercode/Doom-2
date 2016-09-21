9ba9a92d9c06b25a504ad0744ba08d1c143e2586
shortercode <iainshorter@Iains-MacBook-Pro.local>
2016-09-21 10:30:35 +0100

    Reduce clean up
    [reduce] clean up the variables slightly in reduce

--- src/reduce.js
+++ src/reduce.js
@@ -1,16 +1,16 @@
-const whiteSpaceRegex = /^\s*$/;
+const WHITE_SPACE_REGEX = /^\s*$/;
 
 function cleanNode (parentNode) {
 	const childNodes = Array.prototype.slice.call(parentNode);
 	for (let child of childNodes) {
-		if (child.nodeType === Node.TEXT_NODE && whiteSpaceRegex.test(child.textContent))
+		if (child.nodeType === Node.TEXT_NODE && WHITE_SPACE_REGEX.test(child.textContent))
 			parentNode.removeChild(child);
 		else if (child.hasChildNodes())
-			cleanHTML(child);
+			cleanNode(child);
 	}
 }
 
-export default function reduce (element) {
+export function reduce (element) {
 	if (!(element instanceof HTMLElement))
 		throw new TypeError(`Failed to execute 'cleanElement' parameter 1 is not of type 'HTMLElement'`);
 	element.normalize(); // removes completely empty text nodes and joins adjascent text nodes ( minimise our work )
