e0c384281178602152eefa42e215a8aeba937dec
Iain Shorter <iainshorter@gmail.com>
2016-10-11 19:08:48 +0100

    Implemented Remove

--- src/remove.js
+++ src/remove.js
@@ -1,1 +1,31 @@
-// TODO write me!
+import search from './search';
+import { requireType, requireInstance, isType } from './util';
+
+// TODO unfinished 
+function remove(element, delay){
+	
+    if (delay) {
+		requireType(delay, "number");
+        setTimeout(remove, delay, element);
+    } else {
+		
+        if (isType(element, 'string'))
+            element = search(element);
+			
+        if (element[0]) { // array like
+            let i = element.length;
+            while (i--)
+                remove(element[i]);
+			
+			return;
+        }
+		
+		requireInstance(element, HTMLElement);
+		
+        if (element.parentNode)
+            element.parentNode.removeChild(element);
+
+	}
+}
+
+export remove;
