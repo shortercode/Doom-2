ae0b7f3e5db959112f92a19f047e7f4116ca9731
Iain Shorter <iainshorter@gmail.com>
2016-10-11 19:12:09 +0100

    Created AlloyElement base class

--- src/classes/AlloyElement.js
+++ src/classes/AlloyElement.js
@@ -1,78 +1,113 @@
-const NATIVEPROPERTIES = [
-	'id',
-	'children',
-	'childNodes',
-	'parentNode',
-	'firstChild',
-	'previousSibling',
-	'nextSibling',
-	'nodeName',
-	'className',
-	'classList',
-	'lastChild',
-	'style',
-	'textContent',
-	'innerHTML'
-];
+import EventEmitter from './EventEmitter';
 
-const NATIVEMETHODS = [
-	'querySelector',
-	'querySelectorAll',
-	'appendChild',
-	'setAttribute',
-	'hasAttribute',
-	'getAttribute',
-	'removeChild',
-	'remove',
-	'insertBefore',
-	'insertAfter',
-	'addEventListener',
-	'removeEventListener',
-	'contains'
-];
-
-const PROTOTYPE = {
+class Alloy extends EventEmitter {
 	
-};
-
-// Polyfill HTMLElement
-{
-	polyfill(HTMLElement.prototype, 'remove', function remove() {
-		if (this.parentNode) {
-			this.parentNode.removeChild(this);
-		}
-	});
-	polyfill(HTMLElement.prototype, 'insertAfter', function insertAfter() {
-		return this.insertBefore(newNode, referenceNode && referenceNode.nextSibling);
-	});
+	constructor (element) {
+		super();
+		this.element = element;
+	}
 	
-}
+	get id () {
+		return this.element.id;
+	}
+	
+	set id (v) {
+		this.element.id = v;
+	}
+	
+	get class () {
+		return this.element.classList;
+	}
+	
+	get text () {
+		return this.element.textContent;
+	}
+	
+	set text (v) {
+		this.element.textContent = v;
+	}
+	
+	get html () {
+		return this.element.innerHTML;
+	}
+	
+	set html (v) {
+		this.element.innerHTML = v;
+	}
 
-function AlloyElement (element) {
-	if (!(this instanceof Alloy))
-		throw new TypeError(`Class constructor Alloy cannot be invoked without 'new'`);
-	this.element = element;
-}
+	get children () {
+		return this.element.children;
+	}
+	
+	get parent () {
+		return this.element.parentNode;
+	}
+	
+	get firstChild () {
+		return this.element.firstChild;
+	}
+	
+	get lastChild () {
+		return this.element.lastChild;
+	}
+	
+	get previousSibling () {
+		return this.element.previousSibling;
+	}
+	
+	get nextSibling () {
+		return this.element.nextSibling;
+	}
+	
+	get style () {
+		return this.element.style;
+	}
+	
+	search (...args) {
+		return this.element.querySelector(...args);
+	}
 
-for (let property of NATIVEPROPERTIES) {
-	Object.defineProperty(PROTOTYPE, property, {
-		get () {
-			return this.element[property];
-		},
-		set (v) {
-			this.element[property] = v;
-		}
-	});
-}
+	searchAll (...args) {
+		return this.element.querySelectorAll(...args);
+	}
 
-for (let method of NATIVEMETHODS) {
-	Object.defineProperty(PROTOTYPE, method, {
-		value () {
-			return this.element[method].apply(this.element, arguments);
-		}
-	});
-}
+	appendChild (...args) {
+		return this.element.appendChild(...args);
+	}
+	
+	removeChild (...args) {
+		return this.element.removeChild(...args);
+	}
 
-AlloyElement.prototype = PROTOTYPE;
+	remove (...args) {
+		return this.element.remove(...args);
+	}
+
+	insertBefore (...args) {
+		return this.element.insertBefore(...args);
+	}
+
+	insertAfter (...args) {
+		return this.element.insertAfter(...args);
+	}
+
+	setAttribute (...args) {
+		return this.element.setAttribute(...args);
+	}
+
+	hasAttribute (...args) {
+		return this.element.hasAttribute(...args);
+	}
+
+	getAttribute (...args) {
+		return this.element.getAttribute(...args);
+	}
+
+	
+
+	contains (...args) {
+		return this.element.contains(...args);
+	}
+}
 
 export default AlloyElement;
