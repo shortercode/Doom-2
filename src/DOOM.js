fa02198de8f2f80a23bc9abff1d37f07643c1acf
Iain Shorter <iainshorter@gmail.com>
2016-11-08 18:23:31 +0000

    started module wrapper script, for the rollup distributable

---
+++ src/DOOM.js
@@ -0,0 +1,14 @@
+import './classes/EventEmitter';
+import './classes/CompositeDisposable';
+import './classes/Disposable';
+import './classes/Model';
+
+
+import './animate';
+import './create';
+import './define';
+import './goatee';
+import './modify';
+import './reduce';
+import './remove';
+import './search';
