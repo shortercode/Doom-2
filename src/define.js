08af5f92d402ef4a7ad6938356b3ef8ba84a76c5
Iain Shorter <iainshorter@gmail.com>
2016-11-08 18:22:52 +0000

    Wrote experimental custom elements implementation, turns out the browsers aren't ready yet... will remove to sub branch later

--- src/define.js
+++ src/define.js
@@ -2,10 +2,10 @@ import { AlloyElement } from "./classes/AlloyElement"
 
 const DEFINITIONS = new Map();
 
-export function define(name, options) {
-	RequireInstance(options, Object);
+export function define(name, constructor) {
+	RequireInstance(constructor, Function);
 	RequireType(str, 'string');
-	const tag = options.tagName || 'div';
+	const tag = options.tag || 'div';
 	const creator = options.constructor;
 	if (name.length < 1)
 		throw "Invalid name"
@@ -13,11 +13,9 @@ export function define(name, options) {
 	if (DEFINITIONS.has(name))
 		return;
 	// remove argument properties from the options prototype
-	delete options.is;
-	if (options.created)
-		delete options.created;
-	if (options.extends)
-		delete options.extends;
+	delete options.tag;
+	if (options.constructor)
+		delete options.constructor;
 	// create the inherited constructor
 	var constructor = createAlloyConstructor(tag, createdCallback);
 	constructor.name = name;
@@ -44,3 +42,67 @@ function createAlloyConstructor (tagName, creator) {
 		}
 	}
 }
+
+// the wall mk2, much smaller
+const DEFINITIONS = new Map();
+const NATIVE_CLASSES = new Map();
+function getNativeClass (name) {
+	let nativeClass = NATIVE_CLASSES.get(name);
+	if (!nativeClass) {
+		const tempElement = document.createElement(name);
+		nativeClass = tempElement.constructor;
+		NATIVE_CLASSES.set(name, nativeClass);
+	}
+	return nativeClass;
+}
+function define(name, method) {
+	window.customElements.define(name, method, {extends: method.extends});
+	DEFINITIONS.set(name, method);
+	return method;
+}
+function alloy(name) {
+	let newAlloy = DEFINITIONS.get(name);
+	if (!newAlloy) {
+		newAlloy = class AlloyElement extends getNativeClass(name) {
+			constructor () {
+				super();
+				this.eventHandlers = new Map();
+			}
+			on (name, method) {
+				this.eventHandlers.set(name, method);
+			}
+			fire (name, obj) {
+				const handler = this.eventHandlers.get(name);
+				handler && handler(obj);
+			}
+			static get extends () {
+				return name;
+			}
+		};
+		DEFINITIONS.set(name, newAlloy);
+	}
+	return newAlloy;
+}
+function create(name) {
+	const method = DEFINITIONS.get(name);
+	return new method();
+}
+
+define("alloy-button", class FancyButton extends alloy('button') {
+	constructor() {
+		super(); // always call super() first in the ctor.
+		this.addEventListener('click', e => this.drawRipple(e.offsetX, e.offsetY));
+	}
+
+	// Material design ripple animation.
+	drawRipple(x, y) {
+		let div = document.createElement('div');
+		div.classList.add('ripple');
+		this.appendChild(div);
+		div.style.top = `${y - div.clientHeight / 2}px`;
+		div.style.left = `${x - div.clientWidth / 2}px`;
+		div.style.backgroundColor = 'currentColor';
+		div.classList.add('run');
+		div.addEventListener('transitionend', e => div.remove());
+	}
+});
