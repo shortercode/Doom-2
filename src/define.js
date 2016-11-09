268847ae0df0b7a329f5f7cb63f4ef6dba91c1b8
Iain Shorter <iainshorter@gmail.com>
2016-11-09 18:04:29 +0000

    Updated definition method to functions similar to custom-elements proof of concept, also disabled said POC

--- src/define.js
+++ src/define.js
@@ -1,48 +1,48 @@
-import { AlloyElement } from "./classes/AlloyElement"
+import AlloyElement from "./classes/AlloyElement"
 
 const DEFINITIONS = new Map();
+const EXTENDABLE_ALLOYS = new Map();
 
-export function define(name, constructor) {
-	RequireInstance(constructor, Function);
-	RequireType(str, 'string');
-	const tag = options.tag || 'div';
-	const creator = options.constructor;
-	if (name.length < 1)
-		throw "Invalid name"
-	// check that it isn't already defined
+function define(name, create) {
+	
+	RequireInstance(create, Function);
+	RequireType(name, 'string');
+	
+	if (!(create.prototype.instanceof AlloyElement))
+		throw new TypeError(`${create} does not extend ${AlloyElement}`);
+	
+	if (!name.includes("-"))
+		throw new Error(`Failed to define "${name}" in DOOM element registry. AlloyElement names must include a "-"`);
+	
 	if (DEFINITIONS.has(name))
-		return;
-	// remove argument properties from the options prototype
-	delete options.tag;
-	if (options.constructor)
-		delete options.constructor;
-	// create the inherited constructor
-	var constructor = createAlloyConstructor(tag, createdCallback);
-	constructor.name = name;
-	constructor.prototype = Object.create(Alloy.prototype);
-	assign(constructor.prototype, options);
-	// set the constructor on the prototype
-	constructor.prototype.constructor = constructor;
-
-	// statically append the extension to the constructor
-	constructor.extends = tag;
-	// save the definition
-	DEFINITIONS.set(name, constructor);
+		throw new Error(`Failed to define "${name}" in DOOM element registry. "${name}" is already defined in the registry`);
+		
+	DEFINITIONS.set(name, create);
+	EXTENDABLE_ALLOYS.set(name, create);
 }
 
-export function getAlloyDefinition(name) {
+function alloy(name) {
+	let newAlloy = EXTENDABLE_ALLOYS.get(name);
+	if (!newAlloy) {
+		newAlloy = class NativeAlloyElement extends AlloyElement (name) {
+			constructor () {
+				super(document.createElement(name));
+			}
+			static get extends () {
+				return name;
+			}
+		};
+		EXTENDABLE_ALLOYS.set(name, newAlloy);
+	}
+	return newAlloy;
+}
+
+function getAlloyDefinition(name) {
   return DEFINITIONS.get(name);
 }
 
-function createAlloyConstructor (tagName, creator) {
-	return class extends AlloyElement {
-		constructor () {
-			super(document.createElement(tagName));
-			creator && creator.apply(this, arguments);
-		}
-	}
-}
-
+export { getAlloyDefinition, define, alloy };
+/*
 // the wall mk2, much smaller
 const DEFINITIONS = new Map();
 const NATIVE_CLASSES = new Map();
@@ -83,26 +83,4 @@ function alloy(name) {
 	}
 	return newAlloy;
 }
-function create(name) {
-	const method = DEFINITIONS.get(name);
-	return new method();
-}
-
-define("alloy-button", class FancyButton extends alloy('button') {
-	constructor() {
-		super(); // always call super() first in the ctor.
-		this.addEventListener('click', e => this.drawRipple(e.offsetX, e.offsetY));
-	}
-
-	// Material design ripple animation.
-	drawRipple(x, y) {
-		let div = document.createElement('div');
-		div.classList.add('ripple');
-		this.appendChild(div);
-		div.style.top = `${y - div.clientHeight / 2}px`;
-		div.style.left = `${x - div.clientWidth / 2}px`;
-		div.style.backgroundColor = 'currentColor';
-		div.classList.add('run');
-		div.addEventListener('transitionend', e => div.remove());
-	}
-});
+*/
