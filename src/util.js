255f13ff11e59f5d069764ab062519e0f800cd33
Iain Shorter <iainshorter@gmail.com>
2016-12-16 16:47:15 +0000

    Added object checks to util

--- src/util.js
+++ src/util.js
@@ -1,3 +1,7 @@
+function getConstructor(instance) {
+    return Object.getPrototypeOf(instance).constructor;
+}
+
 export function requireType(object, name) {
   if (typeof object !== name)
     throw new TypeError(`${object} is not a ${name}`);
@@ -5,7 +9,7 @@ export function requireType(object, name) {
 
 export function requireInstance(instance, method) {
   if (!(instance instanceof method))
-    throw new TypeError(`${instance} is not a method ${method}`);
+    throw new TypeError(`${instance} is not a ${method}`);
 }
 
 export function isType(object, name) {
@@ -14,4 +18,16 @@ export function isType(object, name) {
 
 export function isInstance(instance, method) {
   return instance instanceof method;
+}
+
+export function isObject(instance) {
+    if (typeof instance !== "object" || x === null)
+        return false;
+    return Object.getPrototypeOf(instance).constructor === Object;
+}
+
+export function requireObject(instance) {
+    if (typeof instance !== "object" || x === null || getConstructor(instance) !== Object)
+        throw new TypeError(`${instance} is not a Object`);
+    return ;
 }
